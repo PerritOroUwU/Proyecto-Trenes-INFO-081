e63f0e72add1b153955887ec0803fecb2ba891ee
Llan <ianbb186@gmail.com>
2025-11-09 21:49:51 -0300

    cambia a sistema guardado

---
+++ logic/sistema_guardado.py
@@ -0,0 +1,12 @@
+class SistemaDeGuardado:
+    def __init__(self):
+        
+
+        def guardar_simulacion(self, estado):
+            pass
+
+
+
+        def cargar_simulacion(self):
+            pass
+#no nos alcanzo el tiempo para poder implementar parcialmente el sistema de guardado o cargado
