16e62cf5f19a3e703e5ce69a57085b5f0393c019
danielaaa7 <danielaantimilla8@gmail.com>
2025-12-01 18:26:06 -0300

    rutasclass

--- models/rutas.py
+++ models/rutas.py
@@ -1,1 +1,16 @@
+class RutasTren:
+    def __init__(self):
+        self.rutas = [
+            "Estacion Central - Rancagua",
+            "Estacion Central - Chillan",
+            "Rancagua - Estacion Central",
+            "Rancagua - Talca",
+            "Talca - Chillan",
+            "Talca - Rancagua",
+            "Chillan - Estacion Central",
+            "Chillan - Talca"
+        ]
+
+    def obtener_rutas(self):
+        return self.rutas
 
