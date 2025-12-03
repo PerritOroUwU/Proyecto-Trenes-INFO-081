aaddd812ba05e098e7636d345f698f9a93c6c1f6
Llan <ianbb186@gmail.com>
2025-12-02 22:32:54 -0300

    Estado sim v1.1

--- logic/estado_simulacion.py
+++ logic/estado_simulacion.py
@@ -4,8 +4,11 @@ import random;
 
 class EstadoSimulacion:
     def __init__(self, fecha_inicio_str= "2015-01-01 07:00:00", semilla=random.randint(0, 10000)):
+        #inicio en donde si no hay una fecha dada, se inicia en 1 de enero de 2015 a las 7:00 am
+        #y poder generar una semilla para poder en un futuro obtener los mismos resultados
         self.fecha_inicio = datetime.strptime(fecha_inicio_str, "%Y-%m-%d %H:%M:%S")
-        self.fecha_actual = self.fecha_inicio
+        self.fecha_actual = self.fecha_inicio 
+        # Revisar si es correcto la fecha obtenida
         random.seed(semilla)
 
         self.historial_eventos = []
@@ -13,3 +16,6 @@ class EstadoSimulacion:
 
         #self.trenes =
         #self.estaciones =
+
+    def avanzar_tiempo(self, minutos):
+        self.fecha_actual += timedelta(minutes=minutos)
